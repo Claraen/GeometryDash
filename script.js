1b1e7387ee0a7715d84bd112077effe46bc7e387
Clara Cecilia Elm Nettesheim <95877025+Claraen@users.noreply.github.com>
2022-02-08 17:56:11 +0000

    Made smaller Functions

--- script.js
+++ script.js
@@ -35,7 +35,6 @@ class Player {
     this.y = startingY;
     this.initialY = startingY;
     this.ySpeed = 0;
-
     this.size = size;
     this.jumpHeight = jumpHeight;
   }
@@ -84,7 +83,6 @@ function setup() {
 function start() {
   var el = document.getElementById("setUp");
   el.style.display = "none";
-  //speed = 6;
   sound.currentTime = 0;
   sound.play();
   frameRate(speed);
@@ -105,7 +103,6 @@ class Obstacle {
     } else if (this.y > this.initialY) {
       this.y = this.initialY;
     }
-
     this.x -= this.speed;
   }
 }
@@ -126,8 +123,6 @@ class Rectangle extends Obstacle {
     rect(this.x, this.y, this.xLength, this.yLength);
     fill('white');
   }
-
-  //&& (this.y - this.yLength) <= player.y
   //sets the value of the global variable "dead" to true, which should trigger the death function
   kill(player) {
     //the collision might be a bit wonky
@@ -186,11 +181,13 @@ function draw() {
   time += (1 / 25);
   textSize(15);
   text("Score: " + Math.floor(time), 525, 25);
-  // asks the player to jump when they press the up arrow
+  checkJump();
+}
+
+function checkJump(){
   if (keyIsPressed && keyCode == 38) {
     playerOne.jump();
   }
-
   if (dead == true) {
     die();
   }
@@ -212,20 +209,12 @@ function handleObstacles(player) {
   let size = random(15) + 10;
   spawnChance *= 1.0005;
   let currentChance = spawnChance * timeBetweenSpawn;
-  //creates new obstacles
-  if (rand < currentChance) {
-    obstacles[obstacles.length] = new Rectangle(size, size, playerY, speed, 650);
-    speed += 0.1;
-    timeBetweenSpawn = 0;
-  } else if (rand < currentChance * 2) {
-    obstacles[obstacles.length] = new Triangle(size, size, playerY + size, speed, 650);
-    speed += 0.1;
-    timeBetweenSpawn = 0;
-  } else {
-    timeBetweenSpawn += 0.02;
-  }
-
+  createObstacle(rand,currentChance,size);
   //updates obstacles and checks their location
+  obstacleUpdate(player);
+}
+
+function obstacleUpdate(player){
   for (let i = 0; i < obstacles.length; i++) {
     if (obstacles[i].x + 100 >= player.x) {
       obstacles[i].move();
@@ -235,22 +224,34 @@ function handleObstacles(player) {
   }
 }
 
+function createObstacle(rand,currentChance,size) {
+  if (rand < currentChance) {
+    obstacleType(new Rectangle(size, size, playerY, speed, 650));
+  } else if (rand < currentChance * 2) {
+    obstacleType(new Triangle(size, size, playerY + size, speed, 650));
+  } else {
+    timeBetweenSpawn += 0.02;
+  }
+}
+
+function obstacleType(shape){
+      obstacles[obstacles.length] = shape;
+    speed += 0.1;
+    timeBetweenSpawn = 0;
+}
+
 function die() {
   var el = document.getElementById("setUp");
   el.style.display = "block";
-  //on death, game freezes, restart button appears
-  //dead = true;
   clear();
- // let restartButton = createButton("Restart");
   fill(220, 20, 60);
-  
-  textSize(50);
-  text("You Are Dead", 150, 250);
-  textSize(15);
-  text("Click 'START!' to play a new game!" , 185, 300);
-  text("Press the 'UP' arrow to jump!" , 195, 325);
-  textSize(30);
-  if (time > oldScore) {
+  printDead();
+  frameRate(0);
+  sound.pause();
+}
+
+function highScore(){
+    if (time > oldScore) {
     text("New High Score: " + Math.floor(time), 300, 50);
     oldScore = Math.floor(time);
     console.log(oldScore);
@@ -259,17 +260,15 @@ function die() {
     text("Score: " + Math.floor(time), 350, 50);
     text("High Score: " + oldScore, 350, 80);
   }
-  screenWidth = window.width;
-  //domRect = element.getBoundingClientRect();
+}
 
- /* restartButton.id = 'restart';
-  restartButton.size(200, 100);
- // restartButton.position((screenWidth - 200) / 2, 320);
-  restartButton.style("font-family", "Bodoni");
-  restartButton.style("font-size", "48px");
-  restartButton.mouseClicked(restartGame);*/
-  frameRate(0);
-  sound.pause();
+function printDead(){
+  textSize(50);
+  text("You Are Dead", 150, 250);
+  textSize(15);
+  text("Click 'START!' to play a new game!", 185, 300);
+  text("Press the 'UP' arrow to jump!", 195, 325);
+  textSize(30);
 }
 
 function restartGame() {
@@ -278,11 +277,7 @@ function restartGame() {
   spawnChance = 0.005;
   speed = initialSpeed;
   fChoice = Math.floor(random(fCol.length));
-  //restart button disappears
- // var b = document.getElementsByTagName('button');
- // b[jankyCounter].style.display = "none";
   start();
-  //jankyCounter++;
   obstacles = [];
   time = 0;
   var c = document.getElements
@@ -293,20 +288,17 @@ function changePlayerColor() {
   pCol = color;
 }
 
-function easy(){
-  //document.getElementById("button").style.background='#000000';
+function easy() {
   speed = 6;
   initialSpeed = 6;
 }
 
-function medium(){
- // document.getElementById("button").style.background='#000000';
+function medium() {
   speed = 10;
   initialSpeed = 10;
 }
 
-function hard(){
-  //document.getElementById("button").style.background='#000000';
+function hard() {
   speed = 12;
   initialSpeed = 12;
 }
