bd83d2288732f6bc35f40ae38d10444fac0dbc4d
Peter Seibel <peter@gigamonkeys.com>
2022-02-09 20:08:35 -0800

    Reordering code.

--- script.js
+++ script.js
@@ -70,35 +70,6 @@ class Player {
   }
 }
 
-function setup() {
-  createCanvas(canvasSize, 2 * canvasSize / 3);
-  player = new Player(50, floorY - 25, 25, 11);
-  frameRate(0);
-}
-
-function start() {
-  hideMenu();
-  initializeState();
-  startSound();
-  frameRate(60);
-}
-
-function initializeState() {
-  obstacles = [];
-  dead = false;
-  speed = initialSpeed;
-  startFrame = frameCount;
-  lastSpawn = frameCount; // we've got to set it to something
-  floorColor = random(floorColors);
-  playerColor = document.getElementById("playerColor").value;
-}
-
-function startSound() {
-  sound.loop = true;
-  sound.currentTime = 0;
-  sound.play();
-}
-
 class Obstacle {
   constructor(speed, x, y, size) {
     this.speed = speed;
@@ -150,43 +121,102 @@ class Ball extends Obstacle {
   }
 }
 
-function p(x, y) {
-  return {x: x, y: y};
+
+//
+// Called from UI
+//
+
+function easy() {
+  initialSpeed = 6;
+}
+
+function medium() {
+  initialSpeed = 10;
+}
+
+function hard() {
+  initialSpeed = 12;
 }
 
 
-// P5 method called once per frame to update the screen.
+//
+// DOM UI manipulation
+//
+
+function showMenu() {
+  document.getElementById("setUp").style.display = "block";
+}
+
+function hideMenu() {
+  document.getElementById("setUp").style.display = "none";
+}
+
+
+//
+// P5 lifecycle methods
+//
+
+function setup() {
+  createCanvas(canvasSize, 2 * canvasSize / 3);
+  player = new Player(50, floorY - 25, 25, 11);
+  frameRate(0);
+}
+
 function draw() {
   updateWorld();
   drawWorld();
 }
 
+
+//
+// Our own lifecycle -- start is called from the start button in the
+// UI and kicks everything else off.
+//
+
+function start() {
+  hideMenu();
+  initializeState();
+  startSound();
+  frameRate(60);
+}
+
+function gameOver() {
+  showMenu();
+  drawGameOver();
+  frameRate(0);
+  sound.pause();
+}
+
+function initializeState() {
+  obstacles = [];
+  dead = false;
+  speed = initialSpeed;
+  startFrame = frameCount;
+  lastSpawn = frameCount; // we've got to set it to something
+  floorColor = random(floorColors);
+  playerColor = document.getElementById("playerColor").value;
+}
+
+function startSound() {
+  sound.loop = true;
+  sound.currentTime = 0;
+  sound.play();
+}
+
+//
+// Updating the state of the world before we redraw.
+//
+
 function updateWorld() {
+  updatePlayer();
+  updateObstacles();
+}
+
+function updatePlayer() {
   if (jumped()) {
     player.jump();
   }
   player.move();
-  updateObstacles();
-}
-
-function drawWorld() {
-  clear();
-  if (dead) {
-    die();
-  } else {
-    fill(floorColor);
-    rect(0, floorY, width, floorY);
-    player.draw();
-    drawObstacles();
-    updateScore();
-    dead = checkHitPlayer();
-  }
-}
-
-function updateScore() {
-  textSize(15);
-  textAlign(RIGHT);
-  text("Score: " + score(), width - 25, 25);
 }
 
 function jumped() {
@@ -196,29 +226,6 @@ function jumped() {
 function updateObstacles() {
   maybeCreateObstacle();
   moveObstacles();
-}
-
-function moveObstacles() {
-  let stillOnScreen = [];
-  for (let o of obstacles) {
-    o.move();
-    if (!o.isOffscreen()) {
-      stillOnScreen.push(o);
-    }
-  }
-  obstacles = stillOnScreen;
-}
-
-function drawObstacles() {
-  obstacles.forEach(o => o.draw());
-}
-
-function checkHitPlayer() {
-  // This could be made more efficient by only checking obstacles
-  // until we get to the first obstacle whose x is greater than the
-  // right edge of the player. But there are never so many obstacles
-  // on the screen at once that that is likely to matter.
-  return obstacles.some(o => o.hitPlayer());
 }
 
 function maybeCreateObstacle() {
@@ -236,16 +243,72 @@ function spawnChance() {
   return 0.0001 * sinceSpawn * (1.0005 ** totalFrames);
 }
 
-function die() {
-  showMenu();
-  fill(220, 20, 60);
-  printDead();
-  showScore(score());
-  frameRate(0);
-  sound.pause();
+function moveObstacles() {
+  let stillOnScreen = [];
+  for (let o of obstacles) {
+    o.move();
+    if (!o.isOffscreen()) {
+      stillOnScreen.push(o);
+    }
+  }
+  obstacles = stillOnScreen;
 }
 
-function showScore(s) {
+
+//
+// Drawing the updated world.
+//
+
+function drawWorld() {
+  clear();
+  if (!dead) {
+    drawFloor();
+    player.draw();
+    obstacles.forEach(o => o.draw());
+    drawScore();
+    dead = checkHitPlayer();
+  } else {
+    gameOver();
+  }
+}
+
+function drawFloor() {
+  fill(floorColor);
+  rect(0, floorY, width, floorY);
+}
+
+function drawScore() {
+  textSize(15);
+  textAlign(RIGHT);
+  text("Score: " + score(), width - 25, 25);
+}
+
+function checkHitPlayer() {
+  // This could be made more efficient by only checking obstacles
+  // until we get to the first obstacle whose x is greater than the
+  // right edge of the player. But there are never so many obstacles
+  // on the screen at once that that is likely to matter.
+  return obstacles.some(o => o.hitPlayer());
+}
+
+function drawGameOver() {
+  fill(220, 20, 60);
+  drawFinalMessage();
+  drawFinalScore(score());
+}
+
+function drawFinalMessage() {
+  let mid = width / 2;
+  textSize(50);
+  textAlign(CENTER);
+  text("You Are Dead", mid, 250);
+  textSize(15);
+  text("Click 'START!' to play a new game!", mid, 300);
+  text("Press the 'UP' arrow to jump!", mid, 325);
+  textSize(30);
+}
+
+function drawFinalScore(s) {
   textAlign(RIGHT);
   let r = width - 25;
   if (s > highScore) {
@@ -257,48 +320,21 @@ function showScore(s) {
   }
 }
 
+//
+// Utility functions
+//
+
+// Compute the score based on the number of frames that have gone by.
 function score() {
   return Math.floor((frameCount - startFrame) / 25);
-}
-
-function printDead() {
-  let mid = width / 2;
-  textSize(50);
-  textAlign(CENTER);
-  text("You Are Dead", mid, 250);
-  textSize(15);
-  text("Click 'START!' to play a new game!", mid, 300);
-  text("Press the 'UP' arrow to jump!", mid, 325);
-  textSize(30);
-}
-
-function easy() {
-  initializeSpeed(6);
-}
-
-function medium() {
-  initializeSpeed(10);
-}
-
-function hard() {
-  initializeSpeed(12);
-}
-
-function initializeSpeed(s) {
-  speed = s;
-  initialSpeed = s;
-}
-
-
-function showMenu() {
-  document.getElementById("setUp").style.display = "block";
-}
-
-function hideMenu() {
-  document.getElementById("setUp").style.display = "none";
 }
 
 // Check whether two ranges overlap.
 function overlapping(low1, high1, low2, high2) {
   return (low1 <= low2 && low2 <= high1) || (low2 <= low1 && low1 <= high2);
+}
+
+// Make a simple point object.
+function p(x, y) {
+  return {x: x, y: y};
 }
